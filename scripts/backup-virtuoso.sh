f4f2319dd782099457eb4cd43b7dca9a96aec846
Erika Pauwels <erika.pauwels@gmail.com>
2017-05-13 17:15:48 +0200

    Convert to docker compose v2

--- scripts/backup-virtuoso.sh
+++ scripts/backup-virtuoso.sh
@@ -11,7 +11,7 @@ cd /data/bravoer/music-crm/scripts
 
 FILENAME="`date +%Y%m%d-%H%M%S`_${APP_NAME}.ttl"
 
-curl -g --data-urlencode query@backup.sparql http://$(docker inspect --format='{{ .NetworkSettings.Networks.bridge.IPAddress }}' musiccrm_db_1):8890/sparql > ${FILENAME}
+curl -g --data-urlencode query@backup.sparql http://$(docker inspect --format='{{ .NetworkSettings.Networks.musiccrm_default.IPAddress }}' musiccrm_db_1):8890/sparql > ${FILENAME}
 
 tar -zcf ${FILENAME}.tar.gz ${FILENAME} 
 curl -T ${FILENAME}.tar.gz ftp://${FTP_HOST}/database-backups/${APP_NAME}/${FILENAME}.tar.gz --user ${FTP_USER}:${FTP_PWD}
