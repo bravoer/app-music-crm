6b9548bbb174ab0ff7fe8e6d3ba9ea5bd07cb246
Erika Pauwels <erika.pauwels@gmail.com>
2018-12-06 17:26:24 +0100

    Update Virtuoso backup script

--- scripts/backup-virtuoso.sh
+++ scripts/backup-virtuoso.sh
@@ -5,18 +5,37 @@ FTP_HOST=ftp.example.com
 FTP_USER=ftpuser
 FTP_PWD=password
 
-APP_NAME=virtuoso
+BACKUP_DIR_SERVICE_NAME=virtuoso
+DAYS=30
 
-cd /data/bravoer/music-crm/scripts
+docker=`which docker`
+if [ $? -ne 0 ]; then
+    echo "ERROR: could not find docker executable";
+    exit -1;
+fi
 
-FILENAME="`date +%Y%m%d-%H%M%S`_${APP_NAME}.ttl"
+# Backup Virtuoso
+virtuoso_container=`docker ps --filter "label=com.docker.compose.project=app-music-crm" --filter "label=com.docker.compose.service=database" --format "{{.Names}}"`
+date=`date +%y%m%dT%H%M`
+backup_name="virtuoso_backup_$date-"
 
-curl -g --data-urlencode query@backup.sparql http://$(docker inspect --format='{{ .NetworkSettings.Networks.musiccrm_default.IPAddress }}' musiccrm_db_1):8890/sparql > ${FILENAME}
+$docker exec -i $virtuoso_container mkdir -p backups
+$docker exec -i $virtuoso_container isql-v <<EOF
+    exec('checkpoint');
+		backup_context_clear();
+		backup_online('$backup_name',30000,0,vector('backups'));
+		exit;
+EOF
+$docker exec -i $virtuoso_container /bin/bash -c "find /data/backups/ -name 'virtuoso_backup_*' -mtime +$DAYS -print0 | xargs -0 rm 2> /dev/null"
 
-tar -zcf ${FILENAME}.tar.gz ${FILENAME} 
-curl -T ${FILENAME}.tar.gz ftp://${FTP_HOST}/database-backups/${APP_NAME}/${FILENAME}.tar.gz --user ${FTP_USER}:${FTP_PWD}
+# FTP upload
+cd /data/bravoer/app-music-crm/scripts
 
-rm ${FILENAME}*
+archive_name="$backup_name-all.tar.gz"
+tar -zcf ${archive_name} ../database/backups/${backup_name}*.bp
+curl -T ${archive_name} ftp://${FTP_HOST}/database-backups/${BACKUP_DIR_SERVICE_NAME}/${archive_name} --user ${FTP_USER}:${FTP_PWD}
+
+rm ${archive_name}
 
 exit 0
 
